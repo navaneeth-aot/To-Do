73211f5c12d89e9f8212a4dae26d957345d78aef
navaneeth-aot <115780441+navaneeth-aot@users.noreply.github.com>
2022-10-26 22:07:29 +0530

    search bug solved

--- To-Do-list-page.js
+++ To-Do-list-page.js
@@ -15,7 +15,8 @@ let indexForEdit;
 let searchTeXt;
 let foundIndex;
 let currentDate;
-
+let filtered = []
+//retrieving data from local storge
 let str = localStorage.getItem("array");
 // convert string to valid object
 let activeArray = JSON.parse(str) || [];
@@ -23,14 +24,14 @@ let activeArray = JSON.parse(str) || [];
 onload();
 //getDate();
 
-
+// addbutton click Event
 addButton.addEventListener("click",() => {
     readData();
     sortByTitle(activeArray , 'arraytitle');
     createTask();
     counting();
 });
-
+//select box change Event
 document.querySelector(".form-select").addEventListener("change", function() {
     if (this.value == "1") {
         sortByTitle(activeArray , 'arraytitle');
@@ -43,11 +44,7 @@ document.querySelector(".form-select").addEventListener("change", function() {
        createCompleteTask()
     }
 });
-
-searchbtn.addEventListener('click',() => {
-    searchByTitle()
-})
-
+//page load function
 function onload() {
     loadAll();
     createTask();
@@ -55,7 +52,7 @@ function onload() {
     sortByTitle(activeArray , 'arraytitle');
     counting();
 }
-
+//All Tasks menu
 function loadAll() {
     document.getElementById("activeContainerHeading").style.display=""
     document.getElementById("active").style.display=""
@@ -64,7 +61,7 @@ function loadAll() {
     createTask();
     createCompleteTask();
 }
-
+//active tasks menu
 function loadActive() {
     document.getElementById("activeContainerHeading").style.display=""
     document.getElementById("active").style.display=""
@@ -72,14 +69,14 @@ function loadActive() {
     document.getElementById("completed").style.display="none"
 
 }
-
+//completed task menu
 function loadCompleted() {
     document.getElementById("activeContainerHeading").style.display="none"
     document.getElementById("active").style.display="none"
     document.getElementById("completedContainerHeading").style.display=""
     document.getElementById("completed").style.display=""
 }
-
+//reading data from input
 function readData() {
     title = document.querySelector("#taskTitle").value;
     let description = document.getElementById("taskdescription").value;
@@ -103,60 +100,45 @@ function readData() {
     
     clear();
 }
-
+//retrieving the index of object from array and store it in an array named filter
 function searchByTitle() {
-    searchText = document.getElementById("searchText").value;
-
-    var found = activeArray.findIndex(function(obj , index) {
-        if(obj.arraytitle.toLowerCase() === searchText.toLowerCase())
-            return true;
+    let searchText = document.getElementById("searchText").value;
+    
+    result = activeArray.filter(function (x,index) {
+     ind = (x.arraytitle.toLowerCase().includes(searchText));
+     if(ind) {
+      filtered.push(index);
+     }
     });
-    
-    foundIndex = found;
-    searchedItem();
-    document.getElementById("searchText").value = ""
-}
 
-function searchedItem() {
-    if(foundIndex == -1) {
-        alert("Data not Available");
+    document.getElementById("active").innerHTML=""
+    document.getElementById("completed").innerHTML=""
+    for(i = 0; i<filtered.length; i++) {
+      searchedActive();
+      searchedCompleted();
     }
-    else {
-        if(activeArray[foundIndex].status == 'active') {
-            document.getElementById("completed").style.display="none";
-            document.getElementById("active").innerHTML=""
-            searchedActive();
-        }
-        if(activeArray[foundIndex].status == 'complete') {
-            document.getElementById("active").style.display="none";
-            document.getElementById("completed").innerHTML=""
-            searchedCompleted();
-        }
-    }
+    filtered = []
 }
-
+// creating active tasks div
 function createTask() {
-    
     document.getElementById("active").innerHTML=""
     for(i=0; i<activeArray.length; i++) {
         active();
     }
 }
-
+// creating complete tasks div
 function createCompleteTask() {
-    
     document.getElementById("completed").innerHTML=""
     for(i=0; i<activeArray.length; i++){
         completed();
     }
 }
-
+// changing the status of the todo tasks according to the checkbox actions
 function markedItem(indexOfcheckbox) {
     var checkBox = document.getElementById(indexOfcheckbox);
     if (checkBox.checked == true) {
         activeArray[indexOfcheckbox].status = "complete"
     }
-
     if (checkBox.checked == false) {
         activeArray[indexOfcheckbox].status = "active"
     }
@@ -167,11 +149,11 @@ function markedItem(indexOfcheckbox) {
     var jsonArr = JSON.stringify(activeArray);
     localStorage.setItem("array", jsonArr);
 }
-
+// initializing the value of delete button globally
 function btnValue(x) {
     value = x;
 }
-
+// deleting the object from array 
 function deleteTask() {
     activeArray.splice(value,1);
     createTask();
@@ -181,7 +163,7 @@ function deleteTask() {
     var jsonArr = JSON.stringify(activeArray);
     localStorage.setItem("array", jsonArr);
 }
-
+// deleteing all the objects which has status = 'complete'
 function deleteCompleted() {
     for(k=0; k<activeArray.length; k++) {
         if(activeArray[k].status == "complete") {
@@ -196,14 +178,14 @@ function deleteCompleted() {
     var jsonArr = JSON.stringify(activeArray);
     localStorage.setItem("array", jsonArr);
 }
-
+// loading the values from array to the edit modal
 function editTask(index) {
     indexForEdit = index;
     document.getElementById("modalTitle").value = activeArray[index].arraytitle;
     document.getElementById("modalDescription").innerText = activeArray[index].description;
     document.getElementById("modalDate").value = activeArray[index].date;
 }
-
+// updating the data that user edited and submitted
 function updateTask() {
   if(!document.getElementById("modalTitle").value) {
     alert("title cannot be null")
@@ -219,16 +201,16 @@ function updateTask() {
     localStorage.setItem("array", jsonArr);
   }
 }
-
+// sort using title
 function sortByTitle (array , key) {
     return array.sort(function(a, b)
     {
-    if (a.arraytitle.toLowerCase() < b.arraytitle.toLowerCase()) return -1;
-    if (a.arraytitle.toLowerCase() > b.arraytitle.toLowerCase()) return 1;
+    if (a.arraytitle.toLowerCase() < b.arraytitle.toLowerCase()) return false;
+    if (a.arraytitle.toLowerCase() > b.arraytitle.toLowerCase()) return true;
     return 0;
     }); 
 }
-
+// sort using Date
 function sortByDate (array , key) {
     return array.sort(function(a, b)
     {
@@ -239,13 +221,13 @@ function sortByDate (array , key) {
     return 0;
     });
 }
-
+// clear contents in input field
 function clear() {
     taskTitle.value = "";
     taskdescription.value = "";
     thedate = "";
 }
-
+// displaying  active todos 
 function active() {
     if(activeArray[i].status == "active") {
         document.getElementById("active").innerHTML +=
@@ -260,7 +242,7 @@ function active() {
                   <a class="titleOfTask text-decoration-none text-black text18" id="titleOfTask">${activeArray[i].arraytitle}</a>
                   <div class="status"></div>
                 </div>
-                <div class="dateAndTime" id="dateAndTime"><img src="/images/calender.png" alt="" class="me-2"> by ${activeArray[i].date}</div>
+                <div class="dateAndTime p-1 rounded" id="dateAndTime"><img src="/images/calender.png" alt="" class="me-2"> by ${activeArray[i].date}</div>
               </div>
             </div>
             <div class="operations d-flex align-self-center gap-4 me-4">
@@ -271,7 +253,7 @@ function active() {
         </div>`
     }
 }
-
+// displaying completed todo 
 function completed() {
     if(activeArray[i].status == "complete") {
         document.getElementById("completed").innerHTML +=
@@ -297,59 +279,59 @@ function completed() {
         </div>`
     }
 }
-
+// displaying todo if searched todo is in active
 function searchedActive() {
-    if(activeArray[foundIndex].status == "active") {
+    if(activeArray[filtered[i]].status == "active") {
         document.getElementById("active").innerHTML +=
         `<div id="tasks">
           <div class="task-item">
             <div class="items">
               <div class="round">
-                <input class = "form-check-input rounded-circle" type = "checkbox" id="${foundIndex}" onclick = "markedItem(this.id)"/>
+                <input class = "form-check-input rounded-circle" type = "checkbox" id="${filtered[i]}" onclick = "markedItem(this.id)"/>
               </div>
               <div class="content">
                 <div class="d-flex align-items-center gap-2">
-                  <a class="titleOfTask text-decoration-none text-black text18" id="titleOfTask">${activeArray[foundIndex].arraytitle}</a>
+                  <a class="titleOfTask text-decoration-none text-black text18" id="titleOfTask">${activeArray[filtered[i]].arraytitle}</a>
                   <div class="status"></div>
                 </div>
-                <div class="dateAndTime gap-1" id="dateAndTime"> <img src="/images/calender.png" alt=""> by ${activeArray[foundIndex].date}</div>
+                <div class="dateAndTime gap-1" id="dateAndTime"> <img src="/images/calender.png" alt=""> by ${activeArray[filtered[i]].date}</div>
               </div>
             </div>
             <div class="operations d-flex align-self-center gap-4 me-4">
-              <i class="bi bi-pencil" data-bs-toggle="modal" data-bs-target="#editModal" id="${foundIndex}" onclick = "editTask(this.id)"></i>
-              <i class="bi bi-trash" data-bs-toggle="modal" data-bs-target="#exampleModal" id="${foundIndex}" onclick = "btnValue(this.id)"></i>
+              <i class="bi bi-pencil" data-bs-toggle="modal" data-bs-target="#editModal" id="${filtered[i]}" onclick = "editTask(this.id)"></i>
+              <i class="bi bi-trash" data-bs-toggle="modal" data-bs-target="#exampleModal" id="${filtered[i]}" onclick = "btnValue(this.id)"></i>
             </div>
           </div>
         </div>`
     }
 }
-
+// displaying todo if searched todo is in complete
 function searchedCompleted() {
-    if(activeArray[foundIndex].status == "complete") {
+    if(activeArray[filtered[i]].status == "complete") {
         document.getElementById("completed").innerHTML +=
         `<div id="tasks">
           <div class="task-item">
             <div class="items">
               <div class="round">
-                <input class = "form-check-input rounded-circle" type = "checkbox" checked id="${foundIndex}" onclick = "markedItem(this.id)"/>
+                <input class = "form-check-input rounded-circle" type = "checkbox" checked id="${filtered[i]}" onclick = "markedItem(this.id)"/>
               </div>
               <div class="content">
                 <div class="d-flex align-items-center gap-2">
-                  <a class="titleOfTask text-decoration-none text-black text18" id="titleOfTask">${activeArray[foundIndex].arraytitle}</a>
-                  <div class="status bg-success"></div>
+                  <a class="titleOfTask text-decoration-none text-black text18" id="titleOfTask">${activeArray[filtered[i]].arraytitle}</a>
+                  <div class="status"></div>
                 </div>
-                <div class="dateAndTime" id="dateAndTime"><img src="/images/calender.png" alt=""> by ${activeArray[foundIndex].date}</div>
+                <div class="dateAndTime gap-1" id="dateAndTime"> <img src="/images/calender.png" alt=""> by ${activeArray[filtered[i]].date}</div>
               </div>
             </div>
             <div class="operations d-flex align-self-center gap-4 me-4">
-              <i class="bi bi-pencil" data-bs-toggle="modal" data-bs-target="#editModal" id="${foundIndex}" onclick = "editTask(this.id)"></i>
-              <i class="bi bi-trash" data-bs-toggle="modal" data-bs-target="#exampleModal" id="${foundIndex}" onclick = "btnValue(this.id)"></i>
+              <i class="bi bi-pencil" data-bs-toggle="modal" data-bs-target="#editModal" id="${filtered[i]}" onclick = "editTask(this.id)"></i>
+              <i class="bi bi-trash" data-bs-toggle="modal" data-bs-target="#exampleModal" id="${filtered[i]}" onclick = "btnValue(this.id)"></i>
             </div>
           </div>
         </div>`
     }
 }
-
+// counting the all todos , active todos and completed todos
 function counting() {
     allCount.innerHTML = "0"
     activeCount.innerHTML = "0"
@@ -364,16 +346,13 @@ function counting() {
         }
     }
 }
-
-function getDate() {
-  var todayDate = new Date();
-  let day = todayDate.getDate();
-  let month = todayDate.getMonth()+1;
-  let year = todayDate.getFullYear();
-  
-  let montharray = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
-  let currentMonth = montharray[month]
-  currentDate = `${day}-${currentMonth}-${year}`
-  document.getElementById("theDate").value = currentDate
-  console.log(currentDate);
-}
+// todays date
+// function getDate() {
+//   var todayDate = new Date();
+//   let day = todayDate.getDate();
+//   let month = todayDate.getMonth();
+//   let year = todayDate.getFullYear();
+//   currentDate = `${day}-${month}-${year}`
+//   document.getElementById("theDate").value = currentDate
+//   console.log(currentDate);;
+// }
